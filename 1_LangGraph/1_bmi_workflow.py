5b1b7a5bea94a9243532f59a916f1ba52b46d22f
ravi0dubey <ravi0dubey@gmail.com>
2025-12-23 06:35:53 -0500

    Adding bmi_category in 1_bmi_workflow.py

--- 1_LangGraph/1_bmi_workflow.py
+++ 1_LangGraph/1_bmi_workflow.py
@@ -8,6 +8,7 @@ class BMIState(TypedDict):
     weight: float  # in kilograms
     height: float  # in meters
     bmi: float     # Body Mass Index
+    category: str  # BMI Category
 
 # Step 2: Define calculate_bmi function
 def calculate_bmi(state: BMIState) -> BMIState:
@@ -16,17 +17,33 @@ def calculate_bmi(state: BMIState) -> BMIState:
     state['bmi'] = round(weight / (height**2),2)
     return state    
 
-# Step 3: Define Graph
+
+# Step 3: Define bmi_category function
+def bmi_category(state: BMIState) -> BMIState:
+    bmi = state['bmi']
+    if bmi < 18.5:
+        state['category'] = 'Underweight'
+    elif 18.5 <= bmi <= 24.99:
+        state['category'] = 'Normal weight'
+    elif 25 <= bmi <= 29.99:
+        state['category'] = 'Overweight'
+    else:
+        state['category'] = 'Obesity'
+    return state
+
+# Step 4: Define Graph
 bmi_graph = StateGraph(BMIState)     
 
-# Step 4: Add Nodes to the graph
+# Step 5: Add Nodes to the graph
 bmi_graph.add_node('calculate_bmi', calculate_bmi)  
+bmi_graph.add_node('bmi_category', bmi_category)  
 
-# Step 5: Add Edges to your graph
+# Step 6: Add Edges to your graph
 bmi_graph.add_edge(START, 'calculate_bmi')
-bmi_graph.add_edge('calculate_bmi', END)    
+bmi_graph.add_edge('calculate_bmi', 'bmi_category')
+bmi_graph.add_edge('bmi_category', END)    
 
-# Step 6: Compile the Graph
+# Step 7: Compile the Graph
 workflow= bmi_graph.compile()   
 
 # Step 7: Execute the Graph with initial data
@@ -39,5 +56,6 @@ initial_data = {
 bmi_result = workflow.invoke(initial_data)
 
 print(f"Bmi: {bmi_result['bmi']}")
+print(f"Category: {bmi_result['category']}")
 
 

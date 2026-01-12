e2fae291e0f36cbf476b56cbe99e261bbd6e7aa8
ravi0dubey <ravi0dubey@gmail.com>
2026-01-12 07:07:16 -0500

    10_presistence_langraph.py

--- 1_LangGraph/10_presistence_langraph.py
+++ 1_LangGraph/10_presistence_langraph.py
@@ -10,3 +10,24 @@ load_dotenv()
 
 # Step1: Declare Model
 model_openapi= ChatOpenAI(model = "gpt-4o-mini",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
+
+
+class JokeState(TypedDict):
+    topic: str
+    joke: str
+    explanation: str
+
+
+def generate_joke(state:JokeState):
+    topic = state["topic"]
+    prompt = f'generate a joke on the topic:{topic}'
+    response = model_openapi.invoke(prompt).content
+    return {'joke': response}
+
+def generate_explanation_on_joke(state: JokeState):
+    joke = state["joke"]
+    prompt = f'generate explanation on the joke: {joke}'
+    response = model_openapi.invoke(prompt).content
+    return {'explanation' : response}
+
+

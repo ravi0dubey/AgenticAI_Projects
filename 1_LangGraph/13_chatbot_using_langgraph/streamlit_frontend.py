3b9062fb573b8d964cad7b3d549ce2b0d287f0b0
ravi0dubey <ravi0dubey@gmail.com>
2026-02-17 07:53:43 -0500

    Adding the changes for chabot

--- 1_LangGraph/13_chatbot_using_langgraph/streamlit_frontend.py
+++ 1_LangGraph/13_chatbot_using_langgraph/streamlit_frontend.py
@@ -7,7 +7,8 @@ CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
-    
+
+# Loading the conversation history  
 for message in  st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
@@ -17,14 +18,19 @@ user_input =  st.chat_input("Type Here" )
 if user_input:
     # first add user message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    
+    # Display user message on the screen
     with st.chat_message('user'):
         st.text(user_input)
 
-    # Step : Add AI message message to message_history
+    #  Get response of the user message by invoking llm from the backend
     response = chat_workflow.invoke({'chat_messages' : [HumanMessage(content= user_input)]}, config= CONFIG)
     ai_message = response['chat_messages'][-1].content
 
+    # Add AI message response to message_history
     st.session_state['message_history'].append({'role': 'AI', 'content': ai_message})
+    
+    # Display AI response message on the screen
     with st.chat_message('AI'):
         st.text(ai_message)  
 
